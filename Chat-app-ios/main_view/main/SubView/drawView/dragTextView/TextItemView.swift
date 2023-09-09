39602f9b591dc5e75558f62c370452c1697977b3
jackson.tmm <ryantokmanmokmtm@hotmail.com>
2023-09-09 22:42:34 +0800

    Added story editor - alignment tool ,scale and rotation

---
+++ Chat-app-ios/main_view/main/SubView/drawView/dragTextView/TextItemView.swift
@@ -0,0 +1,20 @@
+//
+//  TextItem.swift
+//  Chat-app-ios
+//
+//  Created by Jackson.tmm on 3/9/2023.
+//
+
+import SwiftUI
+
+struct TextItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextItem_Previews: PreviewProvider {
+    static var previews: some View {
+        TextItem()
+    }
+}
